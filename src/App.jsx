cd0bf2358c7ec66742641c2575477ffe3dccd926
EricWithC04 <negativegame117@gmail.com>
2024-04-30 16:38:49 -0300

    feat: execute code with a backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import Editor/* , { DiffEditor, useMonaco, loader } */ from '@monaco-editor/react'
 
 function App() {
 
   const monacoRef = useRef(null)
+  const [outPut, setOutPut] = useState('')
 
   function handleEditorChange(value, event) {
     // here is the current value
     console.log("this is the current value:", value);
+    setOutPut(value)
   }
 
   function handleEditorDidMount(editor, monaco) {
@@ -24,18 +26,31 @@ function App() {
     // markers.forEach(marker => console.log('onValidate:', marker.message));
   }
 
+  function handleExecuteCode() {
+    fetch('http://localhost:3000/execute', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        code: outPut
+      })
+    })
+  }
+
   return (
     <>
       <Editor 
         height="90vh" 
         width="90vw" 
-        defaultLanguage='python' 
+        defaultLanguage='javascript' 
         theme='vs-dark'
         onMount={handleEditorDidMount}
         beforeMount={handleEditorWillMount}
         onValidate={handleEditorValidation}
         onChange={handleEditorChange} 
       />
+      <button onClick={handleExecuteCode}>Execute</button>
     </>
   )
 }
