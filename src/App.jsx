960c1010bca5330d93de4ca8a33c35040cf8ba80
EricWithC04 <negativegame117@gmail.com>
2024-05-03 21:40:33 -0300

    feat: editor ready to multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,19 @@ function App() {
   const monacoRef = useRef(null)
   const [outPut, setOutPut] = useState('')
   const [outPut2, setOutPut2] = useState('')
+  const [files, setFiles] = useState([{ name: 'app.py', code: '' }, { name: 'manage.py', code: '' }, { name: 'settings.py', code: '' }, { name: 'urls.py', code: '' }, { name: 'modules.py', code: '' }])
   const [firstOutPut, setFirstOutPut] = useState(true)
+  const [currentFile, setCurrentFile] = useState(0)
 
   function handleEditorChange(value, event) {
     // here is the current value
     console.log("this is the current value:", value);
     console.log(monacoRef.current);
-    firstOutPut ?
-      setOutPut(value) :
-      setOutPut2(value)
+    // firstOutPut ?
+    //   setOutPut(value) :
+    //   setOutPut2(value)
+    const fileWithChanges = files.map((file, index) => index === currentFile ? { ...file, code: value } : file)
+    setFiles(fileWithChanges)
   }
 
   function handleEditorDidMount(editor, monaco) {
@@ -38,7 +42,7 @@ function App() {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        code: firstOutPut ? outPut : outPut2
+        code: files[currentFile].code
       })
     })
   }
@@ -47,29 +51,38 @@ function App() {
     const element = document.createElement("a")
     const file = new Blob([contentFile], {type: 'text/plain'})
     element.href = URL.createObjectURL(file)
-    element.download = `${nameFile}.py`
+    element.download = nameFile
     document.body.appendChild(element)
     element.click()
     document.body.removeChild(element)
   }
 
   return (
-    <>
-      <Editor 
-        height="90vh" 
-        width="90vw" 
-        defaultLanguage='python' 
-        theme='vs-dark'
-        onMount={handleEditorDidMount}
-        beforeMount={handleEditorWillMount}
-        onValidate={handleEditorValidation}
-        onChange={handleEditorChange} 
-        value={firstOutPut ? outPut : outPut2}
-      />
-      <button onClick={() => setFirstOutPut(!firstOutPut)}>Change Code</button>
-      <button onClick={() => handleDownload('app', outPut)}>Export Code</button>
-      <button onClick={handleExecuteCode}>Execute</button>
-    </>
+    <div className='main-bg vw-100 vh-100 d-flex'>
+      <ul className='list-group list-group-flush text-white'>
+        <li onClick={() => setCurrentFile(0)}>app.py</li>
+        <li onClick={() => setCurrentFile(1)}>manage.py</li>
+        <li onClick={() => setCurrentFile(2)}>settings.py</li>
+        <li onClick={() => setCurrentFile(3)}>urls.py</li>
+        <li onClick={() => setCurrentFile(4)}>modules.py</li>
+      </ul>
+      <div>
+        <Editor 
+          height="90vh" 
+          width="90vw" 
+          defaultLanguage='python' 
+          theme='vs-dark'
+          onMount={handleEditorDidMount}
+          beforeMount={handleEditorWillMount}
+          onValidate={handleEditorValidation}
+          onChange={handleEditorChange} 
+          value={files[currentFile].code}
+        />
+        {/* <button onClick={() => setFirstOutPut(!firstOutPut)}>Change Code</button> */}
+        <button onClick={() => handleDownload(files[currentFile].name, files[currentFile].code)}>Export Code</button>
+        <button onClick={handleExecuteCode}>Execute</button>
+      </div>
+    </div>
   )
 }
 
